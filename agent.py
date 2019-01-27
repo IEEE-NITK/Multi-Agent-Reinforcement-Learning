b678a9a6c3ef7e15c51f20194920cca055dbfaca
Moksh Jain <mokshjn00@gmail.com>
2019-01-27 14:59:34 +0530

    add full implementation

--- agent.py
+++ agent.py
@@ -1,34 +1,228 @@
 # Agents for the Switch Riddle Game
 
-import numpy as np 
-import torch
-from torch import optim
+import numpy as np
 import copy
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.nn import functional as F
+from torch.autograd import Variable
+from torch.nn.utils import clip_grad_norm_
+
+
+class CNet(nn.Module):
+  def __init__(self, opts):
+    super(CNet, self).__init__()
+    self.opts = opts
+    self.comm_size = opts['game_comm_bits']
+    self.init_param_range = (-0.08, 0.08)
+
+    ## Lookup tables for the state, action and previous action.
+    self.action_lookup = nn.Embedding(opts['game_nagents'], opts['rnn_size'])
+    self.state_lookup = nn.Embedding(2, opts['rnn_size'])
+    self.prev_action_lookup = nn.Embedding(
+        opts['game_action_space_total'], opts['rnn_size'])
+
+    # Single layer MLP(with batch normalization for improved performance) for producing embeddings for messages.
+    self.message = nn.Sequential(
+        nn.BatchNorm1d(self.comm_size),
+        nn.Linear(self.comm_size, opts['rnn_size']),
+        nn.ReLU(inplace=True)
+    )
+
+    # RNN to approximate the agent’s action-observation history.
+    self.rnn = nn.GRU(
+        input_size=opts['rnn_size'], hidden_size=opts['rnn_size'], num_layers=2, batch_first=True)
+
+    # 2 layer MLP with batch normalization, for producing output from RNN top layer.
+    self.output = nn.Sequential(
+        nn.Linear(opts['rnn_size'], opts['rnn_size']),
+        nn.BatchNorm1d(opts['rnn_size']),
+        nn.ReLU(),
+        nn.Linear(opts['rnn_size'], opts['game_action_space_total'])
+    )
+
+  def get_params(self):
+    return list(self.parameters())
+
+  def reset_parameters(self):
+    """
+    Reset all model parameters
+    """
+    self.rnn.reset_parameters()
+    self.action_lookup.reset_parameters()
+    self.state_lookup.reset_parameters()
+    self.prev_action_lookup.reset_parameters()
+    self.message.apply(weight_reset)
+    self.output.apply(weight_reset)
+    for p in self.rnn.parameters():
+      p.data.uniform_(*self.init_param_range)
+
+  def forward(self, state, messages, hidden, prev_action, agent):
+    state = Variable(torch.LongTensor(state))
+    hidden = Variable(torch.FloatTensor(hidden))
+    prev_action = Variable(torch.LongTensor(prev_action))
+    agent = Variable(torch.LongTensor(agent))
+
+    # Produce embeddings for rnn from input parameters
+    z_a = self.action_lookup(agent)
+    z_o = self.state_lookup(state)
+    z_u = self.prev_action_lookup(prev_action)
+    z_m = self.message(messages.view(-1, self.comm_size))
+
+    # Add the input embeddings to calculate final RNN input.
+    z = z_a + z_o + z_u + z_m
+    z = z.unsqueeze(1)
+
+    rnn_out, h = self.rnn(z, hidden)
+    # Produce final CNet output q-values from GRU output.
+    out = self.output(rnn_out[:, -1, :].squeeze())
+
+    return h, out
+
+
+class DRU:
+	def __init__(self, sigma, comm_narrow=True, hard=False):
+		self.sigma = sigma
+		self.comm_narrow = comm_narrow
+		self.hard = hard
+
+	def regularize(self, m):
+		m_reg = m + torch.randn(m.size()) * self.sigma
+		if self.comm_narrow:
+			m_reg = torch.sigmoid(m_reg)
+		else:
+			m_reg = torch.softmax(m_reg, 0)
+		return m_reg
+
+	def discretize(self, m):
+		if self.hard:
+			if self.comm_narrow:
+				return (m.gt(0.5).float() - 0.5).sign().float()
+			else:
+				m_ = torch.zeros_like(m)
+				if m.dim() == 1:
+					_, idx = m.max(0)
+					m_[idx] = 1.
+				elif m.dim() == 2:
+					_, idx = m.max(1)
+					for b in range(idx.size(0)):
+						m_[b, idx[b]] = 1.
+				else:
+					raise ValueError('Wrong message shape: {}'.format(m.size()))
+				return m_
+		else:
+			scale = 2 * 20
+			if self.comm_narrow:
+				return torch.sigmoid((m.gt(0.5).float() - 0.5) * scale)
+			else:
+				return torch.softmax(m * scale, -1)
+
+	def forward(self, m, train_mode):
+		if train_mode:
+			return self.regularize(m)
+		else:
+			return self.discretize(m)
 
 
 class Agent:
-	def __init__(self, config_options, model, target, agent_id):
-		# Pass configuration parameters using the config_options dictionary
-		self.opt = config_options
+  def __init__(self, opts, game, model, target, agent_no):
+    self.game = game
+    self.opts = opts
+    self.model = model
+    self.model_target = target
+    self.id = agent_no
 
-		# Link model to the agent
-		self.model = model
-		
-		self.model_target = target
+    # Make target model not trainable
+    for param in target.parameters():
+      param.requires_grad = False
 
-		# Means to identify an agent 
-		self.id = agent_id
+    self.episodes = 0
+    self.dru = DRU(opts['game_comm_sigma'])
+    self.optimizer = optim.RMSprop(
+        params=model.get_params(), lr=opts['lr'], momentum=opts['momentum'])
 
-		self.episodes_seen = 0
+  def reset(self):
+    self.model.reset_parameters()
+    self.model_target.reset_parameters()
+    self.episodes = 0
 
-		self.optimizer = # Assign Adam/RMSprop to be used in learn_from_episode
+  def _eps_flip(self, eps):
+    return np.random.rand(self.opts['bs']) < eps
 
+  def _random_choice(self, items):
+    return torch.from_numpy(np.random.choice(items, 1)).item()
 
-		def select_action_and_comm(self, step, q, eps=0, target=False, train_mode = False):
+  def select(self, step, q, eps=0, target=False, train=False):
+    if not train:
+      eps = 0  # Pick greedily during test
 
+    opts = self.opts
 
+    # Get the action range and communication range for the agent for the current time step.
+    action_range, comm_range = self.game.get_action_range(step, self.id)
 
-		def calc_episode_loss(self, episode ):
+    action = torch.zeros(opts['bs'], dtype=torch.long)
+    action_value = torch.zeros(opts['bs'])
+    comm_vector = torch.zeros(opts['bs'], opts['game_comm_bits'])
 
+    select_random_a = self._eps_flip(eps)
+    for b in range(opts['bs']):
+      q_a_range = range(0, opts['game_action_space'])
+      a_range = range(action_range[b, 0].item() - 1, action_range[b, 1].item())
+      if select_random_a[b]:
+        # select action randomly (to explore the state space)
+        action[b] = self._random_choice(a_range)
+        action_value[b] = q[b, action[b]]
+      else:
+        action_value[b], action[b] = q[b, a_range].max(
+            0)  # select action greedily
+      action[b] = action[b] + 1
 
-		def learn_from_episode(self, episode):
+      q_c_range = range(opts['game_action_space'],
+                        opts['game_action_space_total'])
+      if comm_range[b, 1] > 0:
+        # if the agent can communicate for the given time step
+        c_range = range(comm_range[b, 0].item() - 1, comm_range[b, 1].item())
+        # real-valued message from DRU based on q-values
+        comm_vector[b] = self.dru.forward(q[b, q_c_range], train_mode=train)
+    return (action, action_value), comm_vector
+
+  def get_loss(self, episode):
+    opts = self.opts
+    total_loss = torch.zeros(opts['bs'])
+    for b in range(opts['bs']):
+      b_steps = episode.steps[b].item()
+      for step in range(b_steps):
+        record = episode.step_records[step]
+        for i in range(opts['game_nagents']):
+          td_action = 0
+          r_t = record.r_t[b][i]
+          q_a_t = record.q_a_t[b][i]
+
+          # Calculate td loss for environment action
+          if record.a_t[b][i].item() > 0:
+            if record.terminal[b].item() > 0:
+              td_action = r_t - q_a_t
+            else:
+              next_record = episode.step_records[step + 1]
+              q_next_max = next_record.q_a_max_t[b][i]
+              td_action = r_t = opts['gamma'] * q_next_max - q_a_t
+
+          loss_t = td_action ** 2
+          total_loss[b] = total_loss[b] + loss_t
+    loss = total_loss.sum()
+    return loss / (opts['bs'] * opts['game_nagents'])
+
+  def update(self, episode):
+    self.optimizer.zero_grad()
+    loss = self.get_loss(episode)
+    loss.backward()
+    # Clip gradients for stable training
+    clip_grad_norm_(parameters=self.model.get_params(), max_norm=10)
+    self.optimizer.step()
+    self.episodes += 1
+
+    # Update target model
+    if self.episodes % self.opts['step_target'] == 0:
+      self.model_target.load_state_dict(self.model.state_dict())
--- arena.py
+++ arena.py
@@ -1,48 +1,214 @@
-import numpy as np 
+from utils import DotDic
+import copy
 import torch
-
+import torch.nn as nn
+from env.switch_riddle import SwitchGame
+from agent import CNet, DRU, Agent
 
 class Arena:
-	def __init__(self, opt):
-		self.opt = opt
+  def __init__(self, opt, game):
+    self.opt = opt
+    self.game = game
+    self.eps = opt.eps
+
+  def create_episode(self):
+    opt = self.opt
+    episode = DotDic({})
+    episode.steps = torch.zeros(opt.bs).int()
+    episode.ended = torch.zeros(opt.bs).int()
+    episode.r = torch.zeros(opt.bs, opt.game_nagents).float()
+    episode.step_records = []
+
+    return episode
+
+  def create_step_record(self):
+    """
+    Returns an empty step record to store the data from each step in the episode
+    """
+    opt = self.opt
+    record = DotDic({})
+    record.s_t = None
+    record.r_t = torch.zeros(opt.bs, opt.game_nagents)
+    record.terminal = torch.zeros(opt.bs)
+
+    record.agent_inputs = []
+    record.a_t = torch.zeros(opt.bs, opt.game_nagents, dtype=torch.long)
+    record.comm = torch.zeros(opt.bs, opt.game_nagents, opt.game_comm_bits)
+    record.comm_target = record.comm.clone()
+
+    record.hidden = torch.zeros(opt.game_nagents, 2, opt.bs, opt.rnn_size)
+    record.hidden_target = torch.zeros(
+        opt.game_nagents, 2, opt.bs, opt.rnn_size)
+
+    record.q_a_t = torch.zeros(opt.bs, opt.game_nagents)
+    record.q_a_max_t = torch.zeros(opt.bs, opt.game_nagents)
+
+    return record
+
+  def run_episode(self, agents, train_mode=False):
+    opt = self.opt
+    game = self.game
+    game.reset()
+    self.eps = self.eps * opt.eps_decay
+
+    step = 0
+    episode = self.create_episode()
+    s_t = game.get_state()
+    # Intialize step record
+    episode.step_records.append(self.create_step_record())
+    episode.step_records[-1].s_t = s_t
+    episode_steps = train_mode and opt.nsteps + 1 or opt.nsteps
+    while step < episode_steps and episode.ended.sum() < opt.bs:
+      # Run through the episode
+      episode.step_records.append(self.create_step_record())
+
+      for i in range(1, opt.game_nagents + 1):
+        agent = agents[i]
+        agent_idx = i - 1
+
+        # Retrieve model inputs from the records
+        comm = episode.step_records[step].comm.clone()
+        comm_limited = self.game.get_comm_limited(step, agent.id)
+        if comm_limited is not None:
+          comm_lim = torch.zeros(opt.bs, 1, opt.game_comm_bits)
+          for b in range(opt.bs):
+            if comm_limited[b].item() > 0:
+              comm_lim[b] = comm[b][comm_limited[b] - 1]
+          comm = comm_lim
+        else:
+          comm[:, agent_idx].zero_()
+        prev_action = torch.ones(opt.bs, dtype=torch.long)
+        if not opt.model_dial:
+          prev_message = torch.ones(opt.bs, dtype=torch.long)
+        for b in range(opt.bs):
+          if step > 0 and episode.step_records[step - 1].a_t[b, agent_idx] > 0:
+            prev_action[b] = episode.step_records[step - 1].a_t[b, agent_idx]
+        batch_agent_index = torch.zeros(
+            opt.bs, dtype=torch.long).fill_(agent_idx)
+
+        agent_inputs = {
+            'state': episode.step_records[step].s_t[:, agent_idx],
+            'messages': comm,
+            'hidden': episode.step_records[step].hidden[agent_idx, :],
+            'prev_action': prev_action,
+            'agent': batch_agent_index
+        }
+        episode.step_records[step].agent_inputs.append(agent_inputs)
+
+        # Get Q-values from CNet
+        hidden_t, q_t = agent.model(**agent_inputs)
+        episode.step_records[step + 1].hidden[agent_idx] = hidden_t.squeeze()
+        # Pick actions based on q-values
+        (action, action_value), comm_vector = agent.select(
+            step, q_t, eps=self.eps, train=train_mode)
+
+        episode.step_records[step].a_t[:, agent_idx] = action
+        episode.step_records[step].q_a_t[:, agent_idx] = action_value
+        episode.step_records[step + 1].comm[:, agent_idx] = comm_vector
+
+      a_t = episode.step_records[step].a_t
+      episode.step_records[step].r_t, episode.step_records[step].terminal = self.game.step(
+          a_t)
+
+      # Update episode record rewards
+      if step < opt.nsteps:
+        for b in range(opt.bs):
+          if not episode.ended[b]:
+            episode.steps[b] = episode.steps[b] + 1
+            episode.r[b] = episode.r[b] + episode.step_records[step].r_t[b]
+
+          if episode.step_records[step].terminal[b]:
+            episode.ended[b] = 1
+
+      # Update target network during training
+      if train_mode:
+        for i in range(1, opt.game_nagents + 1):
+          agent_target = agents[i]
+          agent_idx = i - 1
+
+          agent_inputs = episode.step_records[step].agent_inputs[agent_idx]
+          comm_target = agent_inputs.get('messages', None)
+
+          comm_target = episode.step_records[step].comm_target.clone()
+          comm_limited = self.game.get_comm_limited(step, agent.id)
+          if comm_limited is not None:
+            comm_lim = torch.zeros(opt.bs, 1, opt.game_comm_bits)
+            for b in range(opt.bs):
+              if comm_limited[b].item() > 0:
+                comm_lim[b] = comm_target[b][comm_limited[b] - 1]
+            comm_target = comm_lim
+          else:
+            comm_target[:, agent_idx].zero_()
+
+          agent_target_inputs = copy.copy(agent_inputs)
+          agent_target_inputs['messages'] = Variable(comm_target)
+          agent_target_inputs['hidden'] = episode.step_records[step].hidden_target[agent_idx, :]
+          hidden_target_t, q_target_t = agent_target.model_target(
+              **agent_target_inputs)
+          episode.step_records[step +
+                               1].hidden_target[agent_idx] = hidden_target_t.squeeze()
+
+          (action, action_value), comm_vector = agent_target.select(
+              step, q_target_t, eps=0, target=True, train=True)
+
+          episode.step_records[step].q_a_max_t[:, agent_idx] = action_value
+          episode.step_records[step +
+                               1].comm_target[:, agent_idx] = comm_vector
+
+      step = step + 1
+      if episode.ended.sum().item() < opt.bs:
+        episode.step_records[step].s_t = self.game.get_state()
+
+    episode.game_stats = self.game.get_stats(episode.steps)
+
+    return episode
+
+  def average_reward(self, episode, normalized=True):
+    reward = episode.r.sum()/(self.opt.bs * self.opt.game_nagents)
+    if normalized:
+      oracle_reward = episode.game_stats.oracle_reward.sum()/self.opt.bs
+      if reward == oracle_reward:
+        reward = 1
+      elif oracle_reward == 0:
+        reward = 0
+      else:
+        reward = reward/oracle_reward
+    return float(reward)
+
+  def train(self, agents, reset=True, verbose=False, test_callback=None):
+    opt = self.opt
+    if reset:
+      for agent in agents[1:]:
+        agent.reset()
+
+    rewards = []
+    for e in range(opt.nepisodes):
+      episode = self.run_episode(agents, train_mode=True)
+      norm_r = self.average_reward(episode)
+      if verbose:
+        print('train epoch:', e, 'avg steps:',
+              episode.steps.float().mean().item(), 'avg reward:', norm_r)
+      agents[1].update(episode)
+
+      if e % opt.step_test == 0:
+        episode = self.run_episode(agents, train_mode=False)
+        norm_r = self.average_reward(episode)
+        rewards.append(norm_r)
+        print('TEST EPOCH:', e, 'avg steps:',
+              episode.steps.float().mean().item(), 'avg reward:', norm_r)
 
 
-	def create_episode(self):
+def main():
+  game = SwitchGame(DotDic(opts))
+  cnet = CNet(opts)
+  cnet_target = copy.deepcopy(cnet)
+  agents = [None]
+  for i in range(1, opts['game_nagents'] + 1):
+    agents.append(Agent(DotDic(opts), game=game, model=cnet, target=cnet_target, agent_no=i))
+
+arena = Arena(DotDic(opts), game)
+arena.train(agents)
 
 
-	def track_step_record(self):
-
-
-	def run_episode(self, agents, is_training = False):
-
-
-
-	def calc_average_reward(self, episode):
-		reward = episode.r.sum()/(self.opt.bs * self.opt.num_agents)
-
-	def train(self, agents, reset_agents = True):
-		opt = self.opt
-
-		if reset_agents:
-			for agent in agents[1:]:
-				agent.reset()
-
-		for ep in range(opt.num_episodes):
-
-			episode = self.run_episode(agents, True)
-			avg_reward = self.calc_average_reward(episode)
-
-			print("Training Epoch: ",e, "Avg Steps: ", episode.steps.float().mean().item(), 'Avg Reward: ',avg_reward)
-
-			# Insert code from Agent class to invoke learn_from_episode here
-
-
-			if e%opt.step_value == 0:
-				episode = self.run_episode(agents, False)
-				avg_reward = self.calc_average_reward(episode)
-				rewards.append(avg_reward)
-
-				print("Testing Epoch: ",e, "Avg Steps: ", episode.steps.float().mean().item(), 'Avg Reward: ',avg_reward)
-
-
-
+if __name__ == '__main__':
+  main()
